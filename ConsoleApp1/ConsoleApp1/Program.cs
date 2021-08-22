62d55c783f31348d926ccc14b50366827294be6c
devendran0077 <devendran0077@gmail.com>
2021-08-21 20:15:01 -0400

    comm

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             //Some changes happened 123 123 123 1234
             Console.WriteLine("Hello World!");
             Console.WriteLine(Add(3, 4));
+            Console.WriteLine(Add(30, 40));
             Console.ReadLine();
         }
         private static  int  Add(int a,int b)
