a1f095633ec16d4300f73a6828fcd3b2a9c72b7b
devendran0078 <devendran0078@gmail.com>
2021-08-22 13:37:28 -0400

    some comment removed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,16 +9,16 @@ namespace ConsoleApp1
             //Some changes happened 123 123 123 1234
             Console.WriteLine("Hello World!");
             Console.WriteLine(Add(3, 4));
-<<<<<<< HEAD
+
             Console.WriteLine(Substarct(3, 4));
             Console.WriteLine(Devide(3, 4));
-=======
+
  Console.WriteLine(Add(30, 40));
             Console.WriteLine(Multiply(3, 4));
             Console.WriteLine(Multiply(30, 40));
 
 
->>>>>>> Test3Dev
+
             Console.ReadLine();
         }
         private static  int  Add(int a,int b)
@@ -30,13 +30,11 @@ namespace ConsoleApp1
         {
             return a + b;
         }
-<<<<<<< HEAD
+
         private static int Devide(int a, int b)
         {
             return a / b;
         }
-=======
-        
->>>>>>> Test3Dev
+
     }
 }
